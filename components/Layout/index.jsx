d475b6299847809b6743607db1f109fca8745fbb
jesuskata <rzarate.alex@gmail.com>
2021-01-19 19:36:30 -0600

    feat(custom-nextjs): 🎸 add styles through module css

--- components/Layout/index.jsx
+++ components/Layout/index.jsx
@@ -5,8 +5,11 @@ import PropTypes from 'prop-types';
 // Components
 import { NavBar } from '../NavBar';
 
+// Styles
+import styles from './layout.module.css';
+
 export const Layout = ({ children }) => (
-  <div>
+  <div className={styles.container}>
     <NavBar />
     {children}
     <footer>This is the footer</footer>
